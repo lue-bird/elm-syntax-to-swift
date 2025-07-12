4b58214a38240e74ae0b26aa67a55a8a2da1c63a
lue-bird <lue.the.bird@gmail.com>
2025-07-12 03:04:15 +0200

    extend sdl2 example

--- example-sdl2/Sources/Run.elm
+++ example-sdl2/Sources/Run.elm
@@ -1,5 +1,30 @@
-module Run exposing (initWindow)
+module Run exposing (initWindow, renderFilledRectangle)
+
+import Color exposing (Color)
+
 
 initWindow : { windowWidth : Int, windowHeight : Int, title : String }
 initWindow =
-    { windowWidth = 800, windowHeight = 600, title = "example-sdl2" }
+    { windowWidth = 800
+    , windowHeight = 600
+    , title = "example-sdl2"
+    }
+
+
+type alias FilledRectangleToRender =
+    { left : Float
+    , top : Float
+    , width : Float
+    , height : Float
+    , color : Color
+    }
+
+
+renderFilledRectangle : FilledRectangleToRender
+renderFilledRectangle =
+    { left = 50
+    , top = 50
+    , width = 100
+    , height = 100
+    , color = Color.rgb255 0 0 100
+    }
--- example-sdl2/Sources/main.swift
+++ example-sdl2/Sources/main.swift
@@ -4,22 +4,60 @@ if SDL_Init(SDL_INIT_VIDEO) != 0 {
     fatalError("SDL could not initialize! SDL_Error: \(String(cString: SDL_GetError()))")
 }
 
-let (title:title, windowHeight:windowHeight, windowWidth:windowWidth) = Elm.Run_initWindow
-let window = SDL_CreateWindow(
-    title,
-    Int32(SDL_WINDOWPOS_CENTERED_MASK),
-    Int32(SDL_WINDOWPOS_CENTERED_MASK),
+let (
+    title:title,
+    windowHeight:windowHeight,
+    windowWidth:windowWidth
+) = Elm.Run_initWindow
+var window: OpaquePointer?
+var renderer: OpaquePointer?
+SDL_CreateWindowAndRenderer(
     Int32(windowWidth),
     Int32(windowHeight),
-    SDL_WINDOW_SHOWN.rawValue)
+    SDL_WINDOW_RESIZABLE.rawValue,
+    &window,
+    &renderer
+)
+SDL_SetWindowTitle(window, title)
+SDL_SetWindowPosition(
+    window,
+    Int32(SDL_WINDOWPOS_CENTERED_MASK),
+    Int32(SDL_WINDOWPOS_CENTERED_MASK)
+)
 
-var shouldQuit = false
-var event = SDL_Event()
+var shouldQuit: Bool = false
+var event: SDL_Event = SDL_Event()
 while !shouldQuit {
     while SDL_PollEvent(&event) > 0 {
         shouldQuit = (event.type == SDL_QUIT.rawValue)
     }
+    SDL_SetRenderDrawColor(renderer, 100, 0, 0, 0)
+    SDL_RenderClear(renderer)
+
+    renderFilledRectangle(renderer, Elm.Run_renderFilledRectangle)
+
+    SDL_RenderPresent(renderer)
+
     SDL_Delay(1000 / 60)
+}
+
+func renderFilledRectangle(
+    _ renderer: OpaquePointer?,
+    _ filledRectToRender: Elm.Run_FilledRectangleToRender
+) {
+    let colorComponents = Elm.Color_toRgba(filledRectToRender.color)
+    SDL_SetRenderDrawColor(
+        renderer,
+        UInt8(colorComponents.red * 255), UInt8(colorComponents.green * 255),
+        UInt8(colorComponents.blue * 255), UInt8(colorComponents.alpha * 255)
+    )
+    var rectToRender: SDL_Rect = SDL_Rect(
+        x: Int32(filledRectToRender.left),
+        y: Int32(filledRectToRender.top),
+        w: Int32(filledRectToRender.width),
+        h: Int32(filledRectToRender.height)
+    )
+    SDL_RenderFillRect(renderer, &rectToRender)
 }
 
 SDL_DestroyWindow(window)
