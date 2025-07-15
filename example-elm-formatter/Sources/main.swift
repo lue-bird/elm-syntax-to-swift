66a0a7a9afb5cd725f9e3ce128b6b425eee70244
lue-bird <lue.the.bird@gmail.com>
2025-07-15 07:29:25 +0200

    example set up elm-formatter

---
+++ example-elm-formatter/Sources/main.swift
@@ -0,0 +1,11 @@
+﻿import Foundation
+
+do {
+    let inputData = try FileHandle.standardInput.readToEnd() ?? Data()
+    let input = String(data: inputData, encoding: .utf8) ?? ""
+    FileHandle.standardOutput.write(
+        Data(Elm.FormatSingleModule_formatSingleModule(input).utf8)
+    )
+} catch let error {
+    print(error.localizedDescription)
+}
